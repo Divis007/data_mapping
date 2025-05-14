7cb48450bb4bc6dc67970ea8e3f12331227fde2d
kchhatbar <kchhatbar@deloitte.com>
2025-05-14 20:04:01 +0530

    commit to test1 branch

--- data_mapper.py
+++ data_mapper.py
@@ -17,6 +17,17 @@ def perform_data_mapping(input_file: str, mapping_file: str, output_file: str) -
     # Create output dataframe
     output_data = pd.DataFrame()
     
+    def extract_domain(email):
+        return email.split('@')[1] if '@' in email else ''
+    
+    def age_category(age):
+        if age < 30:
+            return 'Young'
+        elif age < 45:
+            return 'Middle'
+        else:
+            return 'Senior'
+    
     # Apply mapping rules
     for _, mapping in mapping_config.iterrows():
         source_field = mapping['source_field']
@@ -29,6 +40,16 @@ def perform_data_mapping(input_file: str, mapping_file: str, output_file: str) -
             output_data[target_field] = input_data[source_field].str.upper()
         elif transform_rule == 'lowercase':
             output_data[target_field] = input_data[source_field].str.lower()
+        elif transform_rule == 'first_three_chars':
+            output_data[target_field] = input_data[source_field].str[:3]
+        elif transform_rule == 'extract_domain':
+            output_data[target_field] = input_data[source_field].apply(extract_domain)
+        elif transform_rule == 'age_category':
+            output_data[target_field] = input_data[source_field].apply(age_category)
+        elif transform_rule == 'before_at':
+            output_data[target_field] = input_data[source_field].str.split('@').str[0]
+        elif transform_rule == 'first_letter':
+            output_data[target_field] = input_data[source_field].str[0]
     
     # Save mapped data
     output_data.to_excel(output_file, index=False)
